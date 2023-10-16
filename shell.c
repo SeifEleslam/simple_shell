a7d5089759c9871e66327ba2f17f3816c12d12e0
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-17 02:47:15 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -40,6 +40,7 @@ void init_global_vars(void)
 	int i;
 
 	gArgs = NULL;
+	cmds = NULL;
 	for (i = 0; i < 100; i++)
 		env_vars[i] = NULL;
 }
@@ -48,17 +49,31 @@ void init_global_vars(void)
  * handle_piped_shell - handle signals
  * Return: 0 on Success
  */
-int handle_piped_shell(void)
+int handle_piped_shell()
 {
-	int bytes_read, count;
-	char buffer[1024];
-
+	int total, bytes_read, handled_bytes, len;
+	char *line, *buffer;
+	
 	if (isatty(STDIN_FILENO) != 0)
 		return (1);
-	bytes_read = handled_read(STDIN_FILENO, buffer, sizeof(char) * 1024);
-	if (bytes_read < 0)
-		return (1);
-	command_process(buffer);
+	buffer = NULL, line = NULL, len = 0, total = 0;
+	handled_bytes = 0, status = 0, bytes_read = 1;
+	while (bytes_read != 0)
+	{
+		len += 1024, buffer = (char *)_realloc(buffer, len - 1024, len);
+		bytes_read = handled_read(STDIN_FILENO, buffer + total, 1024);
+		total += bytes_read;
+	}
+	bytes_read = total;
+	line = (char *)_realloc(line, 0, len);
+	if (bytes_read <= 0)
+		return (-1);
+	if (buffer[bytes_read - 1] == '\n')
+		buffer[bytes_read - 1] = '\0';
+	while (handled_bytes < bytes_read)
+		handled_bytes += cp_line(buffer + handled_bytes, line), command_process(line);
+	free(buffer), free(line);
+	exit(handle_status(status));
 	return (0);
 }
 
