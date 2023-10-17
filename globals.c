27345e8f30518391b75ac75be483caa9d4d807b4
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-17 14:19:36 +0300

    Create globals.c

---
+++ globals.c
@@ -0,0 +1,4 @@
+static __attribute__((unused)) char **gArgs;
+static __attribute__((unused)) char **cmds;
+static __attribute__((unused)) char *env_vars[100];
+static __attribute__((unused)) int status;
