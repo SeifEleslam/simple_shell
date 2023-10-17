08074a61b23702de388f18316564d488236ebaa7
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-17 05:27:21 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -5,13 +5,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-extern char **environ;
 static char **gArgs;
+static char **cmds;
 static char *env_vars[100];
+static int status;
+extern char **environ;
 
 /*Handlers*/
 void handle_shell(void);
@@ -20,28 +23,41 @@ void init_global_vars(void);
 
 void handle_unsetenv(void);
 void handle_setenv(void);
-void handle_getenv(void);
 void handle_exit(void);
+void handle_cd(void);
 
 void interrupt_handler(void);
 void signal_handler(int signal_number);
 
 /*Utils*/
 int _strlen(char *s);
-int str_to_int(char *s);
-int contains(char *s, char c);
-int nonexact_strcmp(char *s1, char *s2);
 void str_append(char *from, char *to, int start);
 int _strcmp(char *s1, char *s2);
+int nonexact_strcmp(char *s1, char *s2);
+int contains(char *s, char c);
+
+int str_to_int(char *s);
 int count_words(char *s);
+void remove_char(char *buff, int index);
+int cp_line(char *buff, char *line);
+void *_realloc(void *ptr, int old_size, int new_size);
 
 /*Cycle functions*/
 void alloc_err(void);
 int handled_read(int fd, void *buff, size_t n);
 int handled_write(int fd, void *buff, size_t n);
+int read_whole(char *buff, int start, int *q, int *dq, int arrow);
+int handle_quote(char c, int *q, int *dq);
+int handle_status(int val);
+int get_path(char *paths, char *path);
 
+void free_args(void);
 void free_all(void);
+void free_env(void);
+void free_cmds(void);
+
 int fill_args(int count, char *buffer);
+int fill_cmds(char *buff);
 int _getline(char *buffer);
 
 /*Command functions*/
@@ -49,13 +65,14 @@ void exe_command(void);
 void exe_ncommand(void);
 void command_process(char *buffer);
 void exe_path(void);
-void command_to_path(void);
+
+int command_to_path(void);
 
 /*Env related functions*/
 char *_getenv(char *name, int val_only);
 void _setenv(char *name, char *value);
 void realloc_environ(char *var, int op);
 
-void handle_cd(void);
+/* void write_env(char **environ);*/
 
 #endif
