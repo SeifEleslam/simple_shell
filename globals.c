ca88519644c2a3704fccf245ed73cb3916647c6a
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-18 03:01:52 +0300

    Update globals.c

--- globals.c
+++ globals.c
@@ -2,8 +2,3 @@ __attribute__((unused)) char **gArgs;
 __attribute__((unused)) char **cmds;
 __attribute__((unused)) char *env_vars[100];
 __attribute__((unused)) int status;
-
-int getOne(void)
-{
-	return 1;
-}
