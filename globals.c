3214d7ab5520496e39e3658d86dcabc4daf68f6a
SeifEleslam <111715636+SeifEleslam@users.noreply.github.com>
2023-10-19 11:52:27 +0300

    Update globals.c

--- globals.c
+++ globals.c
@@ -2,6 +2,6 @@ __attribute__((unused)) char **gArgs;
 __attribute__((unused)) char **cmds;
 __attribute__((unused)) char *env_vars[100];
 __attribute__((unused)) char *program_name;
-__attribute__((unused)) char **pipe_data = {NULL, NULL};
+__attribute__((unused)) char *pipe_data[2] = {NULL, NULL};
 __attribute__((unused)) int status;
 __attribute__((unused)) int cmd_count;
